9d80d3845e775b747704820a8c12c758057be107
Walter Belleville <walterbelleville@gmail.com>
2014-12-04 15:41:07 -0800

    created mongoose schema and models

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,11 +1,18 @@
+// Invoke 'strict' JavaScript mode
+'use strict';
+
+// Create a new 'render' controller method
 exports.render = function(req, res) {
-    if(req.session.lastVisit) {
+    // If the session's 'lastVisit' property is set, print it out in the console
+    if (req.session.lastVisit) {
         console.log(req.session.lastVisit);
     }
 
+    // Set the session's 'lastVisit' property
     req.session.lastVisit = new Date();
 
+    // Use the 'response' object to render the 'index' view with a 'title' property
     res.render('index', {
         title: 'Hello World'
-    })
+    });
 };
--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,6 @@
 module.exports = {
   // Development configuration options
+    db: 'mongodb://localhost/myWebsiteMEAN',
     sessionSecret: 'developmentSessionSecret'
 
 };
